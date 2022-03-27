531dd7b6217ddfd7b6d428067963457a5ffb4028
Evgeniy <serganates@gmail.com>
2022-03-27 10:43:06 +0300

    Added Lesson 6

---
+++ Lesson6_HomeWork_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Task1
+{
+    static void Main(string[] args)
+    {
+        Enter(Rnd());
+    }
+    static int Rnd()
+    {
+        Random rnd = new Random();
+        int val = rnd.Next(1, 10);
+        return val;
+    }
+
+    static void Enter(int val)
+    {
+        int count = 0;
+        for (int i = 0; i < val; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x > 0) count++;
+        } 
+        Console.WriteLine($" Count = {count}");
+    }
+    
+}
